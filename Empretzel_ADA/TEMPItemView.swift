03dacbf9a2b57ddb86d0f82c81c4b84d2ffebe60
RaissaParente <raissabparente@gmail.com>
2023-11-10 11:27:24 -0300

    secondary views changed to modals, basic itemview created, upload process in phases

---
+++ Empretzel_ADA/TEMPItemView.swift
@@ -0,0 +1,18 @@
+//
+//  TEMPItemView.swift
+//  Empretzel_ADA
+//
+//  Created by Raissa Parente on 09/11/23.
+//
+
+import SwiftUI
+
+struct TEMPItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TEMPItemView()
+}
