c6fb772c48eee46cbbe3f28101cd4497c35c929f
RaissaParente <raissabparente@gmail.com>
2023-11-13 18:08:18 -0300

    new folders

---
+++ Empretzel_ADA/Views/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  Empretzel_ADA
+//
+//  Created by Raissa Parente on 13/11/23.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
---
+++ Empretzel_ADA/Views/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  Empretzel_ADA
+//
+//  Created by Raissa Parente on 13/11/23.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
