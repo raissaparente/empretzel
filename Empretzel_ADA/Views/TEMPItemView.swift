47acffe9f09f7de0c9c49b7753dd673430386dc5
RaissaParente <raissabparente@gmail.com>
2023-11-14 18:06:24 -0300

    updated item view / created confirmationview ui

--- Empretzel_ADA/Views/TEMPItemView.swift
+++ Empretzel_ADA/Views/TEMPItemView.swift
@@ -13,79 +13,115 @@ struct ItemView: View {
     @State var isNavigationActive = false
     
     var body: some View {
+        
         NavigationStack {
-            VStack {
+            
+            ZStack {
+                Color.lightgray
+                    .ignoresSafeArea()
+
                 VStack {
-                    item.category.icon
-                    Text(item.name.uppercased())
-                        .font(.title3)
-                        .foregroundStyle(.accent)
                     
-                    MakeCapsuleTag(text: "Dono: Mariana", textColor: .accent, borderColor: item.category.color)
-                    
-                    //Descricao
-                    VStack {
-                        Text(item.details)
+                    VStack (spacing: 20) {
+                        item.category.icon
+                        Text(item.name)
+                            .font(.title3)
+                            .bold()
+                            .foregroundStyle(.accent)
+                        
+                        MakeCapsuleTag(text: "Dono: Mariana", textColor: item.category.color, borderColor: item.category.color)
+                        
+                        //Descricao
+                        VStack {
+                            Text(item.details)
+                        }
+                        .padding()
+                        .frame(maxWidth: .infinity, minHeight: 100, alignment: .topLeading)
+                        .background(.lightgray)
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                        
+                        //Tags
+                        HStack {
+                            MakeCapsuleTag(text: item.category.name.uppercased(), textColor: item.category.color, borderColor: item.category.color)
+                            MakeCapsuleTag(text: "Bom estado".uppercased(), textColor: item.category.color, borderColor: item.category.color)
+                        }
+                        
+                        //Instrucoes de emprestimo
+                        VStack (alignment: .leading, spacing: 0) {
+                            MakeRequestInstruction(number: "1", text: "Solicite o empréstimo")
+                            MakeRequestInstruction(number: "2", text: "Aguarde a confirmação da solicitação")
+                            MakeRequestInstruction(number: "3", text: "Entre em contato com o dono do item")
+                        }
+                        .padding(.vertical, 20)
                     }
                     .padding()
-                    .frame(maxWidth: .infinity, minHeight: 100, alignment: .topLeading)
-                    .background(.lightgray)
+                    .background(.white)
                     .clipShape(RoundedRectangle(cornerRadius: 20))
                     
-                    //Tags
-                    HStack {
-                        MakeCapsuleTag(text: item.category.name.uppercased(), textColor: item.category.color, borderColor: item.category.color)
-                        MakeCapsuleTag(text: "Bom estado".uppercased(), textColor: item.category.color, borderColor: item.category.color)
-                    }
+                    Spacer()
                     
-                    //Instrucoes de emprestimo
-                    VStack (alignment: .leading, spacing: 0) {
-                        MakeRequestInstruction(number: "1", text: "Solicite o empréstimo")
-                        MakeRequestInstruction(number: "2", text: "Aguarde a confirmação da solicitação")
-                        MakeRequestInstruction(number: "3", text: "Entre em contato com o dono do item")
+                    Button {
+                        isNavigationActive = true
+                    } label: {
+                        Text("Quero")
+                            .font(.title2)
+                            .frame(maxWidth: .infinity, minHeight: 60)
                     }
-                    .padding(.vertical, 20)
+                    .buttonStyle(.borderedProminent)
+
+                    Spacer()
                 }
                 .padding()
-                .overlay(
-                    RoundedRectangle(cornerRadius: 20)
-                        .stroke(.gray)
-                        .shadow(color: item.category.color, radius: 10)
-                )
-                
-                Button {
-                    isNavigationActive = true
-                } label: {
-                    Text("Quero")
+                .navigationTitle("Item")
+                .navigationBarTitleDisplayMode(.inline)
+                .navigationDestination(isPresented: $isNavigationActive) {
+                    ItemConfirmationView(item: item, displayRequestView: $displayRequestView)
                 }
-                .frame(width: 290, height: 70)
-                .background(item.category.color)
-                .foregroundStyle(.white)
-                .clipShape(
-                    RoundedRectangle(cornerRadius: 20)
-                )
-            }
-            .padding()
-            .navigationTitle("Item")
-            .navigationBarTitleDisplayMode(.inline)
-            .navigationDestination(isPresented: $isNavigationActive) {
-                ItemConfirmationView(displayRequestView: $displayRequestView)
             }
         }
     }
 }
 
 struct ItemConfirmationView: View {
+    let item: Item
     @Binding var displayRequestView: Bool
     
     var body: some View {
-        Text("Pedido confirmado")
-        Text("Agora o dono aprovará seu pedido.")
-            .onAppear {
-                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                    displayRequestView = false
+        
+        ZStack {
+            
+            Color.lightgray
+                .ignoresSafeArea()
+            VStack {
+                VStack (spacing: 40){
+                    item.category.icon
+                    Text(item.name)
+                        .font(.title3)
+                        .bold()
+                        .foregroundStyle(.accent)
+                    
+                    
+                    Text("Solicitação enviada!")
+                        .font(.largeTitle)
+                        .onAppear {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                                displayRequestView = false
+                            }
+                        }
+                    
+                    Image("logopurple")
+                        .resizable()
+                        .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 100)
                 }
+                .padding()
+                .frame(maxWidth: .infinity, minHeight: 500)
+                .background(.white)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
             }
+            .padding()
+            .navigationTitle("Item")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
@@ -97,13 +133,11 @@ struct MakeRequestInstruction: View {
     var body: some View {
         HStack {
             Text(number)
-                .font(.footnote)
                 .bold()
                 .padding(5)
                 .foregroundStyle(.white)
                 .background(Circle().fill(.accent))
             Text(text)
-                .font(.footnote)
                 .italic()
         }
     }
@@ -118,14 +152,14 @@ struct MakeCapsuleTag: View {
         VStack {
             Text(text)
                 .foregroundStyle(textColor)
-                .font(.system(size: 15))
+                .font(.system(size: 13))
         }
         .padding(.horizontal, 10)
         .padding(.vertical, 5)
         .background {
             Capsule(style: .circular)
                 .strokeBorder(borderColor, lineWidth: 0.8)
-                .background(Capsule().fill(.lightgray))
+                .background(Capsule().fill(borderColor.opacity(0.2)))
         }
     }
 }
